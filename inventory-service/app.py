dff5ac0db2c71a3181990d3e1fb3366606535b64
hgyuxix <thoriqbaihaqi872@gmail.com>
2026-07-21 00:13:10 +0700

    Menambahkan Github Actions CI/CD

---
+++ inventory-service/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+items = {}
+
+@app.route("/")
+def home():
+    return {
+        "service": "Inventory Service",
+        "status": "running"
+    }
+
+@app.route("/health")
+def health():
+    return {
+        "status": "ok"
+    }
+
+@app.route("/items", methods=["GET"])
+def get_items():
+    return jsonify(items)
+
+@app.route("/items", methods=["POST"])
+def add_item():
+    data = request.json
+
+    items[data["id"]] = {
+        "name": data["name"],
+        "qty": data["qty"]
+    }
+
+    return jsonify(items[data["id"]]), 201
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
